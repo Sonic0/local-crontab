3b007bf56d84c28c5edca3cdc415c5513702a068
Andrea Salvatori <andrea.salvatori92@gmail.com>
2021-07-09 19:43:34 +0200

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='local-crontab',
-    version='0.2.0',
+    version='0.3.0',
     license='MIT',
     description='Convert local crontabs to UTC crontabs',
     long_description=readme,
